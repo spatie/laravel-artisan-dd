28a26183d7efc31499eee7ed6d5e5c81b0bd880b
Freek Van der Herten <freek@spatie.be>
2025-02-17 11:28:24 +0100

    fix test

--- tests/DdCommandTest.php
+++ tests/DdCommandTest.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Support\Facades\Artisan;
 
-it('will not run if the environment is not local')
-    ->tap(fn () => Artisan::call('dd "app()->environment()"'))
-    ->expect('This command can only run if')->toSeeInConsoleOutput();
+it('will not run if the environment is not local', function() {
+    Artisan::call('dd "app()->environment()"');
+
+   expect('This command can only run if')->toSeeInConsoleOutput();
+});
